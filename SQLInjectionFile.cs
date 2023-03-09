32767558210a95d86e572d150427d45ed78cf015
Jason Ashworth <jasonash@gmail.com>
2023-03-09 09:53:18 -0500

    Create SQLInjectionFile.cs
    This is bad and codacy should catch it.

---
+++ SQLInjectionFile.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class SQLisBAD
+{
+    string connectionString = "Server=.\PDATA_SQLEXPRESS;Database=;User Id=sa;Password=2BeChanged!;";
+
+    public SQLisBAD()
+	{
+	}
+
+	public void InjectSQL(string author)
+	{
+        var queryString = "SELECT Title, Body, Excerpt FROM Post WHERE author = '" + author + "' ORDER BY Published DESC";
+		using (SqlConnection connection = new SqlConnection(connectionString))
+		{
+            SqlCommand command = new SqlCommand(queryString, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            try
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(String.Format("{0}, {1}",
+                    reader["tPatCulIntPatIDPk"], reader["tPatSFirstname"]));// etc
+                }
+            }
+            finally
+            {
+                // Always call Close when done reading.
+                reader.Close();
+            }
+        }
+    }
+}
